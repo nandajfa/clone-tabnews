e5501718e3c87a8332b37c9fb93ef5fd3cf530bd
nandajfa <nanda.jfa@gmail.com>
2024-12-28 19:00:49 -0300

    fix: handle `client` as `undefined` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -9,10 +9,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.log("\n Erro dentro do database.js");
     console.error(error);
     throw error;
   } finally {
-    await client.end();
+    await client?.end();
   }
 }
 async function getNewClient() {
