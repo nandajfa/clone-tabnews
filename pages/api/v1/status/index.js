9c41a35a1a72084793bc4817765ee2a6685b84a4
nandajfa <nanda.jfa@gmail.com>
2025-04-11 21:17:09 -0300

    refactor: padroniza os controllers dos endpoints `/migrations`e `/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,41 +1,36 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors.js";
+import controller from "infra/controller.js";
 
-async function status(request, response) {
-  try {
-    const dbVersion = await database.query("SHOW server_version;");
-    const dbVersionValue = dbVersion.rows[0].server_version;
+const router = createRouter();
 
-    const dbMaxConnection = await database.query("SHOW max_connections;");
-    const dbMaxConnectionValue = dbMaxConnection.rows[0].max_connections;
+router.get(getHandler);
 
-    const dbName = process.env.POSTGRES_DB;
-    const dbOpenedConnections = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [dbName],
-    });
-    const dbOpenedConnectionsValue = dbOpenedConnections.rows[0].count;
+export default router.handler(controller.errorHandlers);
 
-    const updatedAt = new Date().toISOString();
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          version: dbVersionValue,
-          max_connections: parseInt(dbMaxConnectionValue),
-          opened_connections: dbOpenedConnectionsValue,
-        },
+async function getHandler(request, response) {
+  const dbVersion = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersion.rows[0].server_version;
+
+  const dbMaxConnection = await database.query("SHOW max_connections;");
+  const dbMaxConnectionValue = dbMaxConnection.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbOpenedConnectionsValue = dbOpenedConnections.rows[0].count;
+
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionValue),
+        opened_connections: dbOpenedConnectionsValue,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controller");
-    console.error(publicErrorObject);
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
